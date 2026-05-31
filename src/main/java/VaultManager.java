835d5d6374cbbd94359a69effb9bbfae89766d33
Kilian Meddas <kilianmeddas94380@gmail.com>
2026-05-31 14:29:02 +0100

    Good version VaultManager

--- src/main/java/VaultManager.java
+++ src/main/java/VaultManager.java
@@ -8,10 +8,9 @@ import org.json.simple.parser.ParseException;
 
 
 public class VaultManager {
-    static List<Vault> vaults = new ArrayList<>();
+
     static class Colors {
         public static final String RESET = "\u001B[0m";
-
         public static final String RED = "\u001B[31m";
         public static final String GREEN = "\u001B[32m";
         public static final String BLUE = "\u001B[34m";
@@ -20,102 +19,110 @@ public class VaultManager {
 
 
     public static JSONArray loadJson() throws IOException, ParseException {
-        JSONParser jsonParser = new JSONParser();
-        Object obj = jsonParser.parse(new FileReader(filename));
-        return (JSONArray) obj;
+        File f = new File(filename);
+        if (!f.exists() || f.length() == 0) {
+            return new JSONArray(); // Return empty array if file missing or empty
+        }
+
+        JSONParser parser = new JSONParser();
+        return (JSONArray) parser.parse(new FileReader(filename));
     }
 
     public static void saveJson(JSONArray jsonArray) throws IOException {
-        FileWriter file = new FileWriter(filename);
-        file.write(jsonArray.toJSONString()); //What we want to write in it, it's write-in the RAM
-        // file.flush(); // Say "Write now" (if close just after, it's not necessary)
-        file.close(); // Write and close (do auto flush)
-    }
-
-
-
-    private static List<Task> loadTasksFromJson() throws IOException, ParseException {
-        JSONArray jsonArray= loadJson();
-        List<Task> list = new ArrayList<>();
-
-        for (Object o : jsonArray) {
-            Long id = (Long) ((JSONObject) o).get("id");
-            String TaskName = (String) ((JSONObject) o).get("Task");
-            list.add(new Task(id, TaskName));
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(jsonArray.toJSONString());
         }
-        return list;
     }
 
+
+    // Add new credentials
     public static void add(String service, String username, String password) throws IOException, ParseException {
-        boolean already_used = false;
-        boolean to_add = false;
+        JSONArray jsonArray = loadJson();
 
-        if (!vaults.isEmpty()){
+        // Check for duplicates
+        for (Object o : jsonArray) {
+            JSONObject obj = (JSONObject) o;
+            String s = (String) obj.get("service");
+            String u = (String) obj.get("username");
 
-            for (Vault v : vaults){
-                String serviceAndUsername = v.service.toLowerCase() + v.username.toLowerCase();
-                if (serviceAndUsername.equals(service.toLowerCase()+username.toLowerCase())){
-                    System.out.print(Colors.RED + "Credentials already added for : "+ service + Colors.RESET+"\n");
-                    already_used = true;
-                    break;
-                }
-            }
-            if( !already_used){
-                Vault vault = new Vault(service, username, password);
-                vaults.add(vault);
-                to_add = true;
-            }
-        } else{
-            Vault vault = new Vault(service, username, password);
-            vaults.add(vault);
-            to_add = true;
-
-        }
-
-        if (to_add){
-            File f = new File(filename);
-            JSONObject jsonFile = new JSONObject();
-            jsonFile.put("service", service);
-            jsonFile.put("username", username);
-            jsonFile.put("password", password);
-
-            if (!f.exists() || f.length()==0){
-                JSONArray jsonArray = new JSONArray();
-                jsonArray.add(jsonFile);
-                saveJson(jsonArray);
-
-            } else{
-                JSONArray jsonArray = loadJson();
-                jsonArray.add(jsonFile);
-                saveJson(jsonArray);
-
+            if (s.equalsIgnoreCase(service) && u.equalsIgnoreCase(username)) {
+                System.out.println(Colors.RED + "Credentials already exist for: " + service + Colors.RESET);
+                return;
             }
         }
 
+        // Add new entry
+        JSONObject newEntry = new JSONObject();
+        newEntry.put("service", service);
+        newEntry.put("username", username);
+        newEntry.put("password", password);
 
+        jsonArray.add(newEntry);
+        saveJson(jsonArray);
+
+        System.out.println(Colors.GREEN + "Credentials added for: " + service + Colors.RESET);
     }
 
+    // Delete credentials
     public static void delete(String service, String username) throws IOException, ParseException {
         JSONArray jsonArray = loadJson();
-        for (int i = 0; i < jsonArray.size(); i++){
-            String service_i = (String)((JSONObject)jsonArray.get(i)).get("service");
-            String username_i = (String)((JSONObject)jsonArray.get(i)).get("username");
-            String serviceAndUsername = service_i + username_i;
-            if (serviceAndUsername.equals(service+username)){
+        boolean removed = false;
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject obj = (JSONObject) jsonArray.get(i);
+            String s = (String) obj.get("service");
+            String u = (String) obj.get("username");
+
+            if (s.equalsIgnoreCase(service) && u.equalsIgnoreCase(username)) {
                 jsonArray.remove(i);
+                removed = true;
                 break;
             }
         }
-        saveJson(jsonArray);
 
-    }
-    public static void main(String[] args) throws IOException, ParseException {
-        add("eee", "username", "password");
-        add("service", "username", "password");
-        add("service", "username", "password");
-
-        for (Vault v :  vaults){
-            System.out.println(v.service);
+        if (removed) {
+            saveJson(jsonArray);
+            System.out.println(Colors.BLUE + "Deleted credentials for: " + service + Colors.RESET);
+        } else {
+            System.out.println(Colors.RED + "No credentials found for: " + service + Colors.RESET);
         }
+    }
+
+    // Get password
+    public static void get(String service, String username) throws IOException, ParseException {
+        JSONArray jsonArray = loadJson();
+
+        for (Object o : jsonArray) {
+            JSONObject obj = (JSONObject) o;
+            String s = (String) obj.get("service");
+            String u = (String) obj.get("username");
+
+            if (s.equalsIgnoreCase(service) && u.equalsIgnoreCase(username)) {
+                System.out.println("Password: " + obj.get("password"));
+                return;
+            }
+        }
+
+        System.out.println(Colors.RED + "No credentials found" + Colors.RESET);
+    }
+
+    // List all credentials
+    public static List<Vault> list() throws IOException, ParseException {
+        JSONArray jsonArray = loadJson();
+        List<Vault> list = new ArrayList<>();
+
+        if (jsonArray.isEmpty()) {
+            System.out.println(Colors.RED + "No credentials stored" + Colors.RESET);
+            return List.of();
+        }
+
+
+        for (Object o : jsonArray) {
+            JSONObject obj = (JSONObject) o;
+            list.add(new Vault((String) obj.get("service"), (String) obj.get("username"), (String) obj.get("password")));
+
+        }
+
+        return list;
     }
 }
